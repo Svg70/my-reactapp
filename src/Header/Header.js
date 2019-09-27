d4a40f872e6b1848bab7dbf5d1c74112e02395ed
George <collectionpiter@gmail.com>
2019-09-27 08:56:11 +0300

    изменили структуру

---
+++ src/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () =>{
+  return (
+    <div className = "header">
+        <a href="">Home</a>
+        <a href="">New feed</a>
+        <a href="">Messages</a>
+    </div>
+  );
+}
+
+export default Header;
